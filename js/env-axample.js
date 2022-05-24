876dff04ef7ca05a81cdf190d6da611c95e11537
kyok01 <kazutakayokoils@gmail.com>
2022-05-24 20:31:10 +0900

    version1.0

---
+++ js/env-axample.js
@@ -0,0 +1,9 @@
+    const envapiKey = "xxxxx";
+    const envauthDomain = "xxxxx";
+    const envdatabaseURL = "xxxxx";
+    const envprojectId = "xxxxx";
+    const envstorageBucket = "xxxxx";
+    const envmessagingSenderId = "xxxxx";
+    const envappId = "xxxxx";
+
+    export {envapiKey, envauthDomain, envdatabaseURL, envprojectId, envstorageBucket, envmessagingSenderId, envappId};
